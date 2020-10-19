a9992c3b4ec9b8b2366e1d0f4b93959f5c7281b0
emmathuv <72164952+emmathuv@users.noreply.github.com>
2020-10-19 16:59:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,5 @@ include_once("index.html");
 include_once("page2.html");
 include_once("page3.html");
 include_once("page4.html");
-include_once("main.css");git add <filename>
-
+include_once("main.css");
 ?>
